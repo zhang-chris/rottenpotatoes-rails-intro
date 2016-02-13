67e916486d309e7c863995d48b6cf69bb6e87c0d
Chris Zhang <chris.zhang@berkeley.edu>
2016-02-13 01:01:16 +0000

    test redirect pt2

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -32,10 +32,10 @@ class MoviesController < ApplicationController
     end
     
     if redir
-      # if session[:sort] != nil or session[:rating] != nil
+      if session[:sort] != nil or session[:rating] != nil
         flash.keep
         redirect_to movies_path(:sort => @sort, :ratings => @ratings)
-      # end
+      end
     end
 
     
